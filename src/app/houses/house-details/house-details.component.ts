4e59034616676cf4647a353d086655c2f8607243
Kane Punter <kanetheswdev@gmail.com>
2023-12-11 22:39:26 +0100

    removed unnecessary members variable

--- src/app/houses/house-details/house-details.component.ts
+++ src/app/houses/house-details/house-details.component.ts
@@ -5,30 +5,30 @@ import { House } from 'src/app/models/house';
 import { HousesService } from 'src/app/services/houses.service';
 
 @Component({
-  selector: 'app-house-details',
-  templateUrl: './house-details.component.html',
-  styleUrls: ['./house-details.component.scss']
+    selector: 'app-house-details',
+    templateUrl: './house-details.component.html',
+    styleUrls: ['./house-details.component.scss'],
 })
 export class HouseDetailsComponent implements OnInit {
-  house: House| undefined ;
-  members: Character[] = [] ;
+    house: House | undefined;
+    // members: Character[] = [] ;
 
-  constructor(
-    private houseService: HousesService,     
-    private route: ActivatedRoute) { }
+    constructor(
+        private houseService: HousesService,
+        private route: ActivatedRoute
+    ) {}
 
-  ngOnInit() {
-    this.getHouseDetails();
-  }
-  getHouseDetails() {
-    const slug = this.route.snapshot.paramMap.get('slug');
-    if(slug) {
-      this.houseService.getHouseDetails(slug).subscribe((house: House[]) => {
-        this.house = house[0];
-        this.members = this.house.members;
-      });
+    ngOnInit() {
+        this.getHouseDetails();
     }
-  
- }
-
+    getHouseDetails() {
+        const slug = this.route.snapshot.paramMap.get('slug');
+        if (slug) {
+            this.houseService
+                .getHouseDetails(slug)
+                .subscribe((house: House[]) => {
+                    this.house = house[0];
+                });
+        }
+    }
 }
